799fef1477889b6c4dd6dd62d3875fff9a4e6117
James Lim <lim.yangkuang@gmail.com>
2019-10-26 20:46:03 +0800

    Added more in Portfolio

--- sass-training/freeCodeCamp/portfolio/js/main.js
+++ sass-training/freeCodeCamp/portfolio/js/main.js
@@ -1,16 +1,23 @@
-const menuBtn = document.querySelector('.menu-btn');
-const hamburger = document.querySelector('.menu-btn__burger');
-
-let showMenu = false;
-
+var menuBtn = document.querySelector('.menu-btn');
+var hamburger = document.querySelector('.menu-btn__burger');
+var nav = document.querySelector('.nav');
+var menuNav = document.querySelector('.menu-nav');
+var navItems = document.querySelectorAll('.menu-nav__item');
+var showMenu = false;
 menuBtn.addEventListener('click', toggleMenu);
-
 function toggleMenu() {
-    if(!showMenu) {
+    if (!showMenu) {
         hamburger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
+        navItems.forEach(function (item) { return item.classList.add('open'); });
         showMenu = true;
-    } else {
+    }
+    else {
         hamburger.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+        navItems.forEach(function (item) { return item.classList.remove('open'); });
         showMenu = false;
     }
 }
