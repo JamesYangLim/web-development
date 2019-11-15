2f3bc8144591dbcdc56939881189b357060a8363
James Lim <lim.yangkuang@gmail.com>
2019-11-15 17:28:20 +0800

    added dotenv in graphql example

--- graphql-training/server/app.js
+++ graphql-training/server/app.js
@@ -3,18 +3,16 @@ const graphqlHTTP = require('express-graphql');
 const mongoose = require('mongoose');
 const schema = require('./schema/schema'); // define the schema of the graph
 const cors = require('cors');
+const dotenv = require('dotenv');
 
 const app = express();
+dotenv.config();
 
 // allow cross origin request
 app.use(cors());
 
-const dbUsername = process.env.MONGO_DB_USER;
-const dbPassword = process.env.MONGO_DB_PASS;
-const mongodbUri = 'mongodb+srv://'+dbUsername+':'+dbPassword+'@jcluster-vxxig.mongodb.net/test';
-// const mongodbUri = 'mongodb+srv://'+dbUsername+':'+dbPassword+'@jcluster-vxxig.mongodb.net/test?retryWrites=true&w=majority';
 
-mongoose.connect(mongodbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 // mongoose.connect(mongodbUri, { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
   console.log('connected to mongo db');
